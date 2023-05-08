b2d34fc02d21bf68f1383bef25fa44b79b589b07
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-05-07 22:07:19 -0400

    Form updated

--- more_forms/src/components/Form.jsx
+++ more_forms/src/components/Form.jsx
@@ -2,54 +2,103 @@ import React, {useState} from 'react'
 
 const Form = () => {
     const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
+
     const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
+
     const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+        if(e.target.value.length < 1) {
+            setFirstNameError("");
+        } else if(e.target.value.length < 2) {
+            setFirstName("First Name must be at least 2 characters");
+        } else {
+            setFirstNameError("");
+        }
+    }
+
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+        if(e.target.value.length < 1) {
+            setLastNameError("");
+        } else if(e.target.value.length < 2) {
+            setFirstNameError("Last Name must be at least 2 characters");
+        } else {
+            setLastNameError("");
+        }
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 1) {
+            setEmailError("");
+        } else if(e.target.value.length < 5) {
+            setEmailError("Email must be at least 5 characters");
+        } else {
+            setEmailError("");
+        }
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if(e.target.value.length < 1) {
+            setPasswordError("");
+        } else if(e.target.value.length < 8) {
+            setPasswordError("Password must be at least 2 characters");
+        } else {
+            setPasswordError("");
+        }
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        if(e.target.value.length < 1) {
+            setConfirmPasswordError("");
+        } else if(e.target.value.length  != {password}) {
+            setConfirmPasswordError("Passwords must match");
+        } else {
+            setConfirmPasswordError("");
+        }
+    }
 
     return (
         <div>
             <form>
                 <div>
                     <label>First Name:</label>
-                    <input type="text" onChange={(e) => setFirstName(e.target.value)}/>
+                    <input type="text" onChange={handleFirstName}/>
                 </div>
 
                 <div>
                     <label>Last Name:</label>
-                    <input type="text" onChange={(e) => setLastName(e.target.value)}/>
+                    <input type="text" onChange={handleLastName}/>
                 </div>
 
                 <div>
                     <label>Email:</label>
-                    <input type="text" onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="text" onChange={handleEmail}/>
                 </div>
 
                 <div>
                     <label>Password:</label>
-                    <input type="password" onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" onChange={handlePassword}/>
                 </div>
 
                 <div>
                     <label>Confirm Password:</label>
-                    <input type="password" onChange={(e) => setConfirmPassword(e.target.value)}/>
+                    <input type="password" onChange={handleConfirmPassword}/>
                 </div>
             </form>
-        
-            {/* Displaying Form Data */}
-            <div>
-                <p>Your Form Data</p>
-                <label>First Name:</label>
-                <p>{firstName}</p>
-                <label>Last Name:</label>
-                <p>{lastName}</p>
-                <label>Email:</label>
-                <p>{email}</p>
-                <label>Password:</label>
-                <p>{password}</p>
-                <label>Confirm Password:</label>
-                <p>{confirmPassword}</p>
-            </div>
 
         </div>
     )
